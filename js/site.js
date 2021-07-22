c25140517b62296609eb34356b4e9af77753465e
elbiesmith <elbie.smith@gmail.com>
2021-07-22 09:45:04 -0500

    overhaul and undo button

--- js/site.js
+++ js/site.js
@@ -1,8 +1,9 @@
 let data = [3, 2, 4, 7, 6, 9];
 let largest = 0;
 let outputArray = [];
+let darkArray = [];
 let customData = [];
-let styledObj = new Map();
+// let styledObj = new Map();
 
 
 function checkArray() {
@@ -13,9 +14,10 @@ function checkArray() {
             if (data[i] > largest) {
                 largest = data[i];
                 outputArray.push(data[i]);
-                styledObj.set(data[i], 'light')
+                // styledObj.set(data[i], 'light')
             } else {
-                styledObj.set(data[i], 'dark')
+                darkArray.push(data[i]);
+                // styledObj.set(data[i], 'dark')
             }
         }
     } else {
@@ -23,6 +25,8 @@ function checkArray() {
             if (customData[i] > largest) {
                 largest = customData[i];
                 outputArray.push(customData[i]);
+            } else {
+                darkArray.push(customData[i]);
             }
         }
     }
@@ -35,8 +39,11 @@ function checkArray() {
 function displayArray() {
     let numberDisplay = document.getElementById('numberDisplay');
     let result = document.getElementById('result');
+    let resultDark = document.getElementById('resultDark');
+    let resultBtn = document.getElementById('btnResultModal')
     numberDisplay.innerHTML = '[';
-    result.innerHTML = `${outputArray.length} => [`;
+    result.innerHTML = `${outputArray.length} can see the light:<br> [`;
+    resultDark.innerHTML = `${darkArray.length} live in darkness:<br> [`;
 
     if (customData.length == 0) {
         for (let i = 0; i < data.length; i++) {
@@ -57,10 +64,9 @@ function displayArray() {
 
         }
     }
-
-
     numberDisplay.innerHTML += `]`;
 
+    // display light
     for (let i = 0; i < outputArray.length; i++) {
         if (i == 0) {
             result.innerHTML += `${outputArray[i]}`;
@@ -69,16 +75,28 @@ function displayArray() {
         }
     }
     result.innerHTML += `]`;
+
+    // display dark
+    for (let i = 0; i < darkArray.length; i++) {
+        if (i == 0) {
+            resultDark.innerHTML += `${darkArray[i]}`;
+        } else {
+            resultDark.innerHTML += `, ${darkArray[i]}`;
+        }
+    }
+    resultDark.innerHTML += `]`;
+
+    resultBtn.click();
 }
 
 function addValue() {
     let number = parseInt(document.getElementById('newNumber').value);
     let customDataForm = document.getElementById('customData');
-    if (isNaN(number)) {
+    if (isNaN(number) || number<1) {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'Please enter a number.'
+            text: 'Please enter a positive number greater than 0.'
         })
         resetForm();
     } else {
@@ -103,4 +121,19 @@ function colorize() {
     for(let i = 0; i < data.length; i++){
         
     }
+}
+
+function undo(){
+    //remove from array
+    customData.pop();
+    
+    // update screen
+    let newOutput = '';
+    for (let i = 0; i < customData.length; i++){
+        newOutput += `${customData[i]},`
+    }
+
+    let customDataOutput = document.getElementById('customData');
+    customDataOutput.innerHTML = '';
+    customDataOutput.innerHTML = newOutput;
 }
